775f5c71d0cea336b1517293f41bcb071102aa99
Alain PAOLINI <alain.paolini-ext@seloger.com>
2018-06-25 14:52:14 +0200

    initialize opening-hours validator

--- src/app/opening-hours/opening-hours.component.ts
+++ src/app/opening-hours/opening-hours.component.ts
@@ -13,4 +13,38 @@ export class OpeningHoursComponent implements OnInit {
   @Input() placeholderClosingHour: string = '20:00'
 
   ngOnInit() {}
+
+  // @Input() isRequired: boolean = false;
+
+  // ngOnInit(): void {
+  //   this.addValidators();
+  // }
+
+  // addValidators(): void {
+  //   const openingHourValidators = [];
+  //   const closingHourValidators = [];
+
+  //   // validator
+  //   const isOpeningBeforeClosing = () => {
+  //     const momentOpeningHour = moment(this.controlOpeningHour.value, 'HH:mm');
+  //     const momentClosingHour = moment(this.controlClosingHour.value, 'HH:mm');
+  //     return moment(momentOpeningHour).isBefore(momentClosingHour) ?
+  //       null :
+  //       {openingNotBeforeClosing: true}
+  //   };
+
+  //   // set validators
+  //   openingHourValidators.push(isOpeningBeforeClosing);
+  //   if (this.isRequired) {
+  //     openingHourValidators.push(Validators.required);
+  //     closingHourValidators.push(Validators.required);
+  //   }
+  //   this.controlOpeningHour.setValidators(openingHourValidators);
+  //   this.controlClosingHour.setValidators(closingHourValidators);
+
+  //   // recall openingHour validator checking on closingHour changes
+  //   this.controlClosingHour.valueChanges.subscribe(
+  //     _ => this.controlOpeningHour.setValue(this.controlOpeningHour.value)
+  //   )
+  // }
 }
